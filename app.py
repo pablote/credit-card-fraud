cad63489b7bc25d3c32461d683b0b442700d89b1
Pablo Compagni <pablocompagni@gmail.com>
2017-04-09 00:01:25 -0300

    added plotting

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_curve, roc_auc_score
+import matplotlib.pyplot as plt
 
 data = pd.read_csv('credit_card_fraud.csv')
 
@@ -40,4 +41,15 @@ y_test_scores_lr = [x[1] for x in y_test_predict_lr]
 
 fpr, tpr, thresholds = roc_curve(y_test, y_test_scores_lr)
 auc_score = roc_auc_score(y_test, y_test_scores_lr)
-print("AUC score (closer to 1 the better) = " + str(auc_score))
+
+plt.figure()
+plt.plot(fpr, tpr, color='darkorange',
+         lw=2, label='ROC curve (area = %0.2f)' % auc_score)
+plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic')
+plt.legend(loc="lower right")
+plt.show()
